43b14b3c7101cf1cd8076617126eff5ae761f9f4
rohit <rohitphalke2003@gmail.com>
2024-02-09 19:44:57 +0530

    First trial commit

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { register, login,verifyToken } = require('../controllers/authController');
+const { register, login,verifyToken } = require('../controllers/auth');
 
 router.post('/register', register);
 router.post('/login', login);
