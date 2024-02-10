ae93ab2304e8aad0129147d57490f77f5cf85b19
Swastik Verma <swastikverma308@gmail.com>
2024-02-10 20:24:51 +0530

    ProjectDetails Frontend

--- client/src/pages/ProductOwner/ProductOwnerMain.jsx
+++ client/src/pages/ProductOwner/ProductOwnerMain.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Sidebar from '../../components/ProductOwner/Sidebar'
 import ProductOwnerDashboard from './ProductOwnerDashboard'
+import ProjectDetails from '../../components/ProductOwner/ProjectDetails'
 
 export default function ProductOwnerMain() {
     return (
@@ -9,6 +10,8 @@ export default function ProductOwnerMain() {
             <Sidebar>
                 <Routes>
                     <Route path='/' element={<ProductOwnerDashboard />} />
+                    <Route path='/project' element={<ProjectDetails />} />
+                    
                 </Routes>
             </Sidebar>
         </>
