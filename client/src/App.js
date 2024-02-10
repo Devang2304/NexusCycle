008bc431ded9c744f8e055941fb0c234d92d698f
Shubham More <97827118+shubhamore@users.noreply.github.com>
2024-02-10 17:49:08 +0530

    added mui,sidebar, fixed user role admin in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -12,13 +12,14 @@ import About from './pages/About';
 import AdminDashboard from './pages/Admin/AdminDashboard';
 import AdminStakeholder from './pages/Admin/AdminStakeholder';
 import AdminUsers from './pages/Admin/AdminUsers';
+import AdminMain from './pages/Admin/AdminMain';
 
 function App() {
   const { token, setToken, isAuth, setIsAuth, setUser, user } = useContext(UserContext)
 
-  useEffect(()=>{
-    console.log("user=",user)
-  },[user])
+  useEffect(() => {
+    console.log("user=", user)
+  }, [user])
 
   return (
     <div className="App">
@@ -32,12 +33,8 @@ function App() {
         </>
       }
       {
-        user.role === 'admin' && <>
-          <Routes>
-            <Route path='/' element={<AdminDashboard />} />
-            <Route path='/allusers' element={<AdminUsers />} />
-            <Route path='/allstakeholder' element={<AdminStakeholder />} />
-          </Routes>
+        user !== null && user.role === 'admin' && <>
+          <AdminMain />
         </>
       }
 
---
+++ client/src/pages/Admin/AdminMain.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import AdminDashboard from './AdminDashboard'
+import AdminStakeholder from './AdminStakeholder'
+import AdminUsers from './AdminUsers'
+import Sidebar from '../../components/Admin/Sidebar'
+
+export default function AdminMain() {
+    return (<>
+        <Sidebar>
+            <Routes>
+                <Route path='/' element={<AdminDashboard />} />
+                <Route path='/allusers' element={<AdminUsers />} />
+                <Route path='/allstakeholder' element={<AdminStakeholder />} />
+            </Routes>
+        </Sidebar>
+    </>
+    )
+}
