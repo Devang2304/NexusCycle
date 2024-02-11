d5aab20081c2f0f7f61204d1a63e392770fdcf9a
Shubham More <97827118+shubhamore@users.noreply.github.com>
2024-02-11 11:32:49 +0530

    added video call for all roles

--- client/src/pages/ScrumMaster/ScrumMasterMain.jsx
+++ client/src/pages/ScrumMaster/ScrumMasterMain.jsx
@@ -3,6 +3,8 @@ import { Route, Routes } from 'react-router-dom'
 import Sidebar from '../../components/ScrumMaster/Sidebar'
 import ScrumMasterDashboard from './ScrumMasterDashboard'
 import ProjectList from '../../components/ScrumMaster/ProjectList'
+import DailyScrum from '../../components/ScrumMaster/DailyScrum'
+import VideoCall from './VideoCall'
 
 export default function ScrumMasterMain() {
     return (
@@ -12,6 +14,9 @@ export default function ScrumMasterMain() {
                     <Route path='/' element={<ScrumMasterDashboard />} />
                     {/* param route with project id to give details of the project              <<<<-------------EDIT HERE*/}
                     <Route path='/xyz' element={<ProjectList />} />
+                    
+                <Route path='/video-call/' element={<DailyScrum/>}/>
+                <Route path='/video-call/:roomID' element={<VideoCall />} />
                 </Routes>
             </Sidebar>
         </>
